7227638ee33e7253c5eccdf58c2e06cc34646846
wpuipress <120018841+wpuipress@users.noreply.github.com>
2023-09-27 00:33:07 +0100

    Split out common lists into seperate files

---
+++ assets/js/uip/v3.5/lists/border_positions.js
@@ -0,0 +1,22 @@
+export default {
+  solid: {
+    icon: 'border_outer',
+    value: 'solid',
+  },
+  left: {
+    icon: 'border_left',
+    value: 'left',
+  },
+  top: {
+    icon: 'border_top',
+    value: 'top',
+  },
+  right: {
+    icon: 'border_right',
+    value: 'right',
+  },
+  bottom: {
+    icon: 'border_bottom',
+    value: 'bottom',
+  },
+};
