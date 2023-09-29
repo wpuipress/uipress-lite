3994050fe42a51ef4995b04aa7008536e1995c91
wpuipress <120018841+wpuipress@users.noreply.github.com>
2023-09-29 18:23:05 +0100

    Added confirm component and new functions file

---
+++ assets/js/uip/v3.5/utility/confirm.js
@@ -0,0 +1,102 @@
+import { defineAsyncComponent, nextTick } from '../../../libs/vue-esm-dev.js';
+
+export default {
+  components: {
+    modal: defineAsyncComponent(() => import('./modal.min.js?ver=3.2.12')),
+  },
+  data() {
+    return {
+      title: undefined,
+      message: undefined,
+      okButton: undefined,
+      cancelButton: __('Cancel', 'uipress-lite'),
+      icon: 'report',
+      resolvePromise: undefined,
+      rejectPromise: undefined,
+      disableTeleport: false,
+    };
+  },
+  methods: {
+    /**
+     * Shows confirm dialog and sets options
+     *
+     * @param {Object} opts - the confirm options
+     * @since 3.2.13
+     */
+    show(opts = {}) {
+      this.title = opts.title;
+      this.message = opts.message;
+      this.okButton = opts.okButton;
+      this.disableTeleport = opts.disableTeleport;
+      if (opts.icon) {
+        this.icon = opts.icon;
+      }
+      if (opts.cancelButton) {
+        this.cancelButton = opts.cancelButton;
+      }
+      // Once we set our config, we tell the popup modal to open
+      this.$refs.popup.open();
+
+      nextTick(() => {
+        this.$refs.confirmBody.focus();
+      });
+      // Return promise so the caller can get results
+      return new Promise((resolve, reject) => {
+        this.resolvePromise = resolve;
+        this.rejectPromise = reject;
+      });
+    },
+    /**
+     * Confirms and closes modal
+     *
+     * @since 3.2.13
+     */
+    confirm() {
+      this.$refs.popup.close();
+      this.resolvePromise(true);
+    },
+    /**
+     * Cancels and closes modal
+     *
+     * @since 3.2.13
+     */
+    cancel() {
+      this.$refs.popup.close();
+      this.resolvePromise(false);
+    },
+  },
+  template: `
+  
+	  <modal ref="popup" :disableTeleport="disableTeleport">
+		<div ref="confirmBody" @keydown.escape="cancel()" @keydown.enter="confirm()" class="uip-max-w-320 uip-flex uip-flex-column uip-row-gap-s uip-padding-m" tabindex="1" autofocus>
+		
+		  <div class="uip-flex uip-gap-xs uip-flex-start uip-flex-no-wrap">
+			
+			<h2 class="uip-margin-remove uip-flex-grow">{{ title }}</h2>
+			<div @click="cancel()" class="uip-padding-xxs uip-border-rounder hover:uip-background-muted uip-cursor-pointer">
+			  <span class="uip-icon uip-link-muted">close</span>
+			</div>
+		  
+		  </div>
+		  
+		  <div class="uip-text-muted">{{ message }}</div>
+		  
+		  <div class="uip-flex uip-gap-s">
+		  
+			  <button class="uip-button-default uip-flex uip-gap-xs uip-flex-center" @click="cancel">
+				<span>{{ cancelButton }}</span>
+				<div class="uip-background-grey uip-padding-left-xxs uip-padding-right-xxs uip-border-rounder uip-flex uip-flex-middle">
+				  <span class="uip-text-muted uip-text-s">esc</span>
+				</div>
+			  </button>
+			  
+			  <button class="uip-button-primary uip-flex uip-gap-s uip-flex-center" @click="confirm">
+				<span>{{ okButton }}</span>
+				<span class="uip-icon uip-text-muted uip-dark-mode">keyboard_return</span>
+			  </button>
+		  </div>
+		
+		</div>
+	</modal>
+		`,
+};
---
+++ assets/js/uip/v3.5/utility/functions.js
@@ -0,0 +1,91 @@
+/**
+ * Checks if a nested object exists. if not, it creates each step
+ *
+ * @param {Object} obj - the object to check against
+ * @param {Array} keys - The nested keys to check
+ * @since 3.2.13
+ */
+export function ensureNestedObject(obj, ...keys) {
+  keys.reduce((acc, key, index, arr) => {
+    if (!acc[key]) acc[key] = {};
+    return acc[key];
+  }, obj);
+}
+
+/**
+ * Checks if a nested object exists. Returns false if not, true if so
+ *
+ * @param {Object} obj - the object to check against
+ * @param {Array} keys - The nested keys to check
+ * @returns {Boolean | Mixed} - returns false on path doesn't exist. Returns value otherwise
+ * @since 3.2.13
+ */
+export function hasNestedPath(obj, ...keys) {
+  for (let key of keys) {
+    if (obj.hasOwnProperty(key)) {
+      obj = obj[key];
+    } else {
+      return false; // or undefined, or any other suitable value to indicate "not found"
+    }
+  }
+  return obj;
+}
+
+/**
+ * Preps JSON data for saving to db
+ *
+ * @param {Object | Array} data - the data to prepare
+ * @since 3.2.13
+ */
+export function prepareJSON(data) {
+  const handler = (k, v) => {
+    if (v === 'true' || v === true) return 'uiptrue';
+    if (v === 'false' || v === false) return 'uipfalse';
+    if (v === '') return 'uipblank';
+    return v;
+  };
+  return JSON.stringify(data, handler);
+}
+
+/**
+ * Preps JSON data for saving to db
+ *
+ * @param {Object | Array} data - the data to prepare
+ * @since 3.2.13
+ */
+export function deepClone(data) {
+  const string = JSON.stringify(data);
+  return JSON.parse(string);
+}
+
+/**
+ * Copies text to clipboard
+ *
+ * @param {String} textToCopy - The text to copy
+ * @since 3.2.13
+ */
+export async function copyToClipboard(textToCopy) {
+  // Navigator clipboard api needs a secure context (https)
+  if (navigator.clipboard && window.isSecureContext) {
+    await navigator.clipboard.writeText(textToCopy);
+  } else {
+    // Use the 'out of viewport hidden text area' trick
+    const textArea = document.createElement('textarea');
+    textArea.value = textToCopy;
+
+    // Move textarea out of the viewport so it's not visible
+    textArea.style.position = 'absolute';
+    textArea.style.left = '-999999px';
+
+    document.body.prepend(textArea);
+    textArea.select();
+
+    try {
+      document.execCommand('copy');
+    } catch (error) {
+      console.error(error);
+    } finally {
+      textArea.remove();
+    }
+  }
+}
