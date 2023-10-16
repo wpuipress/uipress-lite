cf1d13675b587cf295d01c950b182fa13c63ceb4
wpuipress <120018841+wpuipress@users.noreply.github.com>
2023-10-16 20:40:28 +0100

    Further improvements to admin menu handling

--- admin/classes/Scripts/AdminMenu.php
+++ admin/classes/Scripts/AdminMenu.php
@@ -88,8 +88,9 @@ class AdminMenu
     $returner = [];
 
     foreach ($menu as $key => $item) {
+      $temp = [];
+
       foreach ($item as $index => $subitem) {
-        $temp = [];
         $type = strpos($subitem[4] ?? '', 'wp-menu-separator') !== false ? 'sep' : 'item';
         $name = $subitem[2] . ($subitem === 'sep' ? $subitem[4] : $subitem[1]);
         $subitem['uip_uid'] = hash('ripemd160', $name);
@@ -159,6 +160,17 @@ class AdminMenu
     return null;
   }
 
+  /**
+   * Looks at all items within the admin menu and updates given item if match is found
+   *
+   * allows updated counts and name changes
+   *
+   * @param object $item - the item to update
+   * @param object $mastermenu object containing all items
+   *
+   * @return updated item if match, original item if no match
+   * @since 3.2.0
+   */
   private static function maybe_replace_item_with_original($item, $mastermenu)
   {
     // If it's not a custom menu we don't need to do this
@@ -179,10 +191,11 @@ class AdminMenu
       return $item;
     }
 
-    $custom = isset($item['custom']) ? $item['custom'] : ($custom = new \stdClass());
+    $custom = isset($item['custom']) ? $item['custom'] : new \stdClass();
 
     $firstFoundItem['custom'] = $custom;
-    error_log('item updated');
+    $firstFoundItem['submenu'] = isset($item['submenu']) ? $item['submenu'] : false;
+
     return $firstFoundItem;
   }
 
@@ -210,240 +223,197 @@ class AdminMenu
     $returnmenu = [];
     $returnsubmenu = [];
 
+    // Handle top level items
     foreach ($menu as $key => $item) {
-      // Replace the item with original if it exists:
-      self::maybe_replace_item_with_original($item, $mastermenu);
-
-      $admin_is_parent = false;
-      $class = [];
-      $aria_attributes = '';
-      $aria_hidden = '';
-      $is_separator = false;
-
-      if ($first) {
-        $class[] = 'wp-first-item';
-        $first = false;
+      // Check whether user has access to item, if not continue
+      if (!current_user_can($item[1])) {
+        continue;
       }
 
-      $submenu_items = [];
-      if (!empty($submenu[$item[2]])) {
-        $class[] = 'wp-has-submenu';
-        $submenu_items = $submenu[$item[2]];
-      }
-
-      $submenu_items = (array) $submenu_items;
-      $submenu_items = Objects::convertObjectsToArrays($submenu_items);
-
-      if (($parent_file && $item[2] === $parent_file) || (empty($typenow) && $self === $item[2])) {
-        if (!empty($submenu_items)) {
-          $class[] = 'wp-has-current-submenu wp-menu-open';
-          $item['active'] = false;
-        } else {
-          $class[] = 'current';
-          $aria_attributes .= 'aria-current="page"';
-          $item['active'] = false;
-        }
-      } else {
-        $class[] = 'wp-not-current-submenu';
-        $item['active'] = false;
-        if (!empty($submenu_items)) {
-          $aria_attributes .= 'aria-haspopup="true"';
-        }
-      }
-
-      if (!empty($item[4])) {
-        $class[] = esc_attr($item[4]);
-      }
-
-      $class = implode(' ', $class);
-      $id = !empty($item[5]) ? ' id="' . preg_replace('|[^a-zA-Z0-9_:.]|', '-', $item[5]) . '"' : '';
-      $img = '';
-      $img_style = '';
-      $img_class = ' dashicons-before';
-
-      if (false !== strpos($class, 'wp-menu-separator')) {
-        $is_separator = true;
-      }
-
-      $title = wptexturize($item[0]);
-      $nameParts = explode('<', $item[0]);
-      if ($nameParts[0] != '') {
-        $strippedName = $nameParts[0];
-      } else {
-        $strippedName = $item[0];
-      }
-      $notifications = self::extractNumberFromHtml($title);
-      if (!is_null($notifications)) {
-        $item['notifications'] = $notifications;
-      }
-
-      // Hide separators from screen readers.
-      if ($is_separator) {
-        $aria_hidden = ' aria-hidden="true"';
-
-        $item['type'] = 'sep';
-
-        if (isset($menu_item['name'])) {
-          $item['name'] = $strippedName;
-        }
-      } else {
-        $item['id'] = $item[5];
-        $item['name'] = html_entity_decode($strippedName);
-        $item['icon'] = self::get_menu_icon($item);
-        $item['classes'] = $class;
-        $item['type'] = 'menu';
-      }
-
-      if (!$is_separator && $submenu_as_parent && !empty($submenu_items)) {
-        $submenu_items = array_values($submenu_items); // Re-index.
-        $menu_hook = get_plugin_page_hook($submenu_items[0][2], $item[2]);
-        $menu_file = $submenu_items[0][2];
-        $pos = strpos($menu_file, '?');
-
-        if (false !== $pos) {
-          $menu_file = substr($menu_file, 0, $pos);
-        }
-
-        if (!empty($menu_hook) || ('index.php' !== $submenu_items[0][2] && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !file_exists(ABSPATH . "/wp-admin/$menu_file"))) {
-          $admin_is_parent = true;
-          $item['url'] = 'admin.php?page=' . $submenu_items[0][2];
-        } else {
-          $item['url'] = $submenu_items[0][2];
-        }
-      } elseif (!empty($item[2]) && current_user_can($item[1])) {
-        $menu_hook = get_plugin_page_hook($item[2], 'admin.php');
-        $menu_file = $item[2];
-        $pos = strpos($menu_file, '?');
-
-        if (false !== $pos) {
-          $menu_file = substr($menu_file, 0, $pos);
-        }
-
-        if (!empty($menu_hook) || ('index.php' !== $item[2] && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !file_exists(ABSPATH . "/wp-admin/$menu_file"))) {
-          $admin_is_parent = true;
-          $item['url'] = 'admin.php?page=' . $item[2];
-        } else {
-          $item['url'] = $item[2];
-        }
-      }
-
-      if (!$is_separator) {
-        ///CREATE UNIQUE ID FOR MENU ITEMS
-        $item['uid'] = hash('ripemd160', $item['id'] . $item['url']);
-      }
-
-      if (!empty($submenu_items)) {
-        $first = true;
-        $tempsub = [];
-
-        foreach ($submenu_items as $sub_key => $sub_item) {
-          $sub_item['active'] = false;
-
-          if (!current_user_can($sub_item[1])) {
-            continue;
-          }
-
-          $class = [];
-          $aria_attributes = '';
-
-          if ($first) {
-            $class[] = 'wp-first-item';
-            $first = false;
-          }
-
-          $menu_file = $item[2];
-          $pos = strpos($menu_file, '?');
-
-          if (false !== $pos) {
-            $menu_file = substr($menu_file, 0, $pos);
-          }
-
-          // Handle current for post_type=post|page|foo pages, which won't match $self.
-          $self_type = !empty($typenow) ? $self . '?post_type=' . $typenow : 'nothing';
-
-          if (isset($submenu_file)) {
-            if ($submenu_file === $sub_item[2]) {
-              $class[] = 'current';
-              $aria_attributes .= ' aria-current="page"';
-            }
-            // If plugin_page is set the parent must either match the current page or not physically exist.
-            // This allows plugin pages with the same hook to exist under different parents.
-          } elseif (
-            (!isset($plugin_page) && $self === $sub_item[2]) ||
-            (isset($plugin_page) && $plugin_page === $sub_item[2] && ($item[2] === $self_type || $item[2] === $self || file_exists($menu_file) === false))
-          ) {
-            $class[] = 'current';
-            $aria_attributes .= ' aria-current="page"';
-          }
-
-          if (!empty($sub_item[4])) {
-            $class[] = esc_attr($sub_item[4]);
-          }
-
-          $class = $class ? ' class="' . implode(' ', $class) . '"' : '';
-
-          $menu_hook = get_plugin_page_hook($sub_item[2], $item[2]);
-          $sub_file = $sub_item[2];
-          $pos = strpos($sub_file, '?');
-          if (false !== $pos) {
-            $sub_file = substr($sub_file, 0, $pos);
-          }
-
-          $title = wptexturize($sub_item[0]);
-
-          if ($aria_attributes != '') {
-            $sub_item['active'] = false;
-          }
-
-          if (!empty($menu_hook) || ('index.php' !== $sub_item[2] && file_exists(WP_PLUGIN_DIR . "/$sub_file") && !file_exists(ABSPATH . "/wp-admin/$sub_file"))) {
-            // If admin.php is the current page or if the parent exists as a file in the plugins or admin directory.
-            if ((!$admin_is_parent && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !is_dir(WP_PLUGIN_DIR . "/{$item[2]}")) || file_exists($menu_file)) {
-              $sub_item_url = add_query_arg(['page' => $sub_item[2]], $item[2]);
-            } else {
-              $sub_item_url = add_query_arg(['page' => $sub_item[2]], 'admin.php');
-            }
-
-            $sub_item_url = $sub_item_url;
-            $sub_item['url'] = $sub_item_url;
-          } else {
-            $sub_item['url'] = $sub_item[2];
-          }
-
-          $title = $sub_item[0];
-          $nameParts = explode('<', $sub_item[0]);
-
-          if ($nameParts[0] != '') {
-            $strippedName = $nameParts[0];
-          } else {
-            $strippedName = html_entity_decode(strip_tags($sub_item[0]));
-          }
-
-          $notifications = preg_replace('/[^0-9]/', '', strip_tags($title));
-
-          if (is_numeric($notifications)) {
-            $sub_item['notifications'] = $notifications;
-          }
-
-          $sub_item['name'] = $strippedName;
-          $sub_item['id'] = $item['id'] . $sub_item['url'];
-          $sub_item['type'] = 'menu';
-          $sub_item['uid'] = hash('ripemd160', $sub_item['id'] . $sub_item['url']);
-          array_push($tempsub, $sub_item);
-        }
-
-        $item['submenu'] = $tempsub;
-      }
-      $submenu_items = [];
-      if (!empty($submenu[$item[2]])) {
-        $returnsubmenu[$item[2]] = $tempsub;
-      }
-
-      array_push($returnmenu, $item);
+      $returnmenu[] = self::handle_top_level_items($item, $mastermenu);
     }
 
     $returner['menu'] = $returnmenu;
 
     return $returner;
+  }
+
+  /**
+   * Processes top level menu items
+   *
+   * @param object $item
+   * @param object $mastermenu
+   *
+   * @returns formatted item
+   * @since 3.2.0
+   *
+   */
+  private static function handle_top_level_items($item, $mastermenu)
+  {
+    // Set up vars
+    $self = $mastermenu['self'];
+    $parent_file = $mastermenu['parent_file'];
+    $submenu_file = $mastermenu['submenu_file'];
+    $plugin_page = $mastermenu['plugin_page'];
+    $typenow = $mastermenu['typenow'];
+    $menu = $mastermenu['menu'];
+    $submenu = (array) $mastermenu['submenu'];
+
+    // Replace the item with original if it exists:
+    $item = self::maybe_replace_item_with_original($item, $mastermenu);
+
+    $admin_is_parent = false;
+    $class = $item[4];
+    $aria_attributes = '';
+    $aria_hidden = '';
+    $item['active'] = false;
+    $subkey = $item[2];
+
+    $submenu_items = !empty($submenu[$subkey]) ? (array) $submenu[$subkey] : [];
+
+    $id = !empty($item[5]) ? ' id="' . preg_replace('|[^a-zA-Z0-9_:.]|', '-', $item[5]) . '"' : '';
+
+    // Check for sep
+    $is_separator = false !== strpos($class, 'wp-menu-separator') ? true : false;
+
+    // Set item type
+    $item['type'] = $is_separator ? 'sep' : 'menu';
+
+    // exit early if sep
+    if ($is_separator) {
+      return $item;
+    }
+
+    $title = wptexturize($item[0]);
+    $nameParts = explode('<', $item[0]);
+    $strippedName = $nameParts[0] != '' ? $nameParts[0] : $item[0];
+    $notifications = self::extractNumberFromHtml($title);
+
+    $item['notifications'] = !is_null($notifications) ? $notifications : 0;
+    $item['id'] = $item[5];
+    $item['name'] = html_entity_decode($strippedName);
+    $item['icon'] = self::get_menu_icon($item);
+    $item['classes'] = $class;
+
+    // If has a submenu
+    if (!empty($submenu_items)) {
+      $submenu_items = array_values($submenu_items); // Re-index.
+      $firstSubmenuItem = (array) $submenu_items[0];
+      $firstSubmenuKey = $firstSubmenuItem[2];
+
+      $menu_hook = get_plugin_page_hook($firstSubmenuKey, $item[2]);
+      $menu_file = $firstSubmenuKey;
+      $pos = strpos($menu_file, '?');
+      $menu_file = $pos !== false ? substr($menu_file, 0, $pos) : $menu_file;
+
+      $conditions = !empty($menu_hook) || ('index.php' !== $firstSubmenuKey && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !file_exists(ABSPATH . "/wp-admin/$menu_file"));
+      $admin_is_parent = $conditions ? true : false;
+      $item['url'] = $conditions ? "admin.php?page={$firstSubmenuKey}" : $firstSubmenuKey;
+
+      // Process submenu
+      $item['submenu'] = self::process_sub_menu_items($submenu_items, $item, $admin_is_parent, $mastermenu);
+    }
+
+    // No submenu
+    if (empty($submenu_items) && !empty($item[2])) {
+      $menu_hook = get_plugin_page_hook($item[2], 'admin.php');
+      $menu_file = $item[2];
+      $pos = strpos($menu_file, '?');
+
+      $menu_file = $pos !== false ? substr($menu_file, 0, $pos) : $menu_file;
+      $conditions = !empty($menu_hook) || ('index.php' !== $item[2] && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !file_exists(ABSPATH . "/wp-admin/$menu_file"));
+
+      $admin_is_parent = $conditions ? true : false;
+      $item['url'] = $conditions ? "admin.php?page={$item[2]}" : $item[2];
+    }
+
+    return $item;
+  }
+
+  /**
+   * Handles and processes submenu items
+   *
+   * @param array $submenu
+   * @param boolean $parentItem
+   * @param boolean $admin_is_parent
+   * @param object $mastermenu
+   *
+   * @return arrays of subitems
+   * @since 3.2.0
+   */
+  private static function process_sub_menu_items($submenu, $parentItem, $admin_is_parent, $mastermenu)
+  {
+    $typenow = $mastermenu['typenow'];
+    $submenu_file = $mastermenu['submenu_file'];
+    $self = $mastermenu['self'];
+    $processed = [];
+
+    foreach ($submenu as $sub_key => $sub_item) {
+      $sub_item = (array) $sub_item;
+      // Cut if no access
+      if (!current_user_can($sub_item[1])) {
+        continue;
+      }
+
+      $class = isset($sub_item[4]) ? $sub_item[4] : '';
+
+      // Check for sep
+      $is_separator = false !== strpos($class, 'wp-menu-separator') ? true : false;
+
+      // exit early if sep
+      if ($is_separator) {
+        $processed[] = $sub_item;
+        continue;
+      }
+
+      $sub_item = self::maybe_replace_item_with_original($sub_item, $mastermenu);
+      $sub_item['active'] = false;
+
+      $class = [];
+      $aria_attributes = '';
+
+      $menu_file = $parentItem[2];
+      $pos = strpos($menu_file, '?');
+
+      $menu_file = $pos !== false ? substr($menu_file, 0, $pos) : $menu_file;
+
+      // Handle current for post_type=post|page|foo pages, which won't match $self.
+      $self_type = !empty($typenow) ? $self . '?post_type=' . $typenow : 'nothing';
+
+      $menu_hook = get_plugin_page_hook($sub_item[2], $parentItem[2]);
+      $sub_file = $sub_item[2];
+      $pos = strpos($sub_file, '?');
+      $sub_file = $pos !== false ? substr($sub_file, 0, $pos) : $sub_file;
+      $title = wptexturize($sub_item[0]);
+
+      $conditions = !empty($menu_hook) || ('index.php' !== $sub_item[2] && file_exists(WP_PLUGIN_DIR . "/$sub_file") && !file_exists(ABSPATH . "/wp-admin/$sub_file"));
+      if ($conditions) {
+        // If admin.php is the current page or if the parent exists as a file in the plugins or admin directory.
+        $adminIsParentConditions = (!$admin_is_parent && file_exists(WP_PLUGIN_DIR . "/$menu_file") && !is_dir(WP_PLUGIN_DIR . "/{$parentItem[2]}")) || file_exists($menu_file);
+        $sub_item['url'] = $adminIsParentConditions ? add_query_arg(['page' => $sub_item[2]], $parentItem[2]) : add_query_arg(['page' => $sub_item[2]], 'admin.php');
+      }
+
+      $sub_item['url'] = !$conditions ? $sub_item[2] : $sub_item['url'];
+
+      $title = wptexturize($sub_item[0]);
+      $nameParts = explode('<', $title);
+      $strippedName = $nameParts[0] != '' ? $nameParts[0] : $sub_item[0];
+      $notifications = self::extractNumberFromHtml($title);
+      $sub_item['notifications'] = !is_null($notifications) ? $notifications : 0;
+
+      $sub_item['name'] = $strippedName;
+      $sub_item['id'] = $parentItem['id'] . $sub_item['url'];
+      $sub_item['type'] = 'menu';
+
+      // Recursively handle sub subs
+      if (isset($sub_item['submenu']) && !empty($sub_item['submenu'])) {
+        $sub_item['submenu'] = self::process_sub_menu_items($sub_item['submenu'], $parentItem, $admin_is_parent, $mastermenu);
+      }
+
+      $processed[] = $sub_item;
+    }
+
+    return $processed;
   }
 
   /**
@@ -473,10 +443,8 @@ class AdminMenu
     $theicon = '';
     $wpicon = $menu_item[6];
 
-    if (isset($menu_item['icon'])) {
-      if ($menu_item['icon'] != '') {
-        return "<span class='uk-icon-button' uk-icon='icon:{$menu_item['icon']};ratio:0.8'></span>";
-      }
+    if (isset($menu_item['icon']) && $menu_item['icon'] != '') {
+      return "<span class='uk-icon-button' uk-icon='icon:{$menu_item['icon']};ratio:0.8'></span>";
     }
 
     if (isset($icons[$wpicon])) {
@@ -491,7 +459,6 @@ class AdminMenu
         return "<span class='uip-icon uip-icon-medium'>{$strippedIcon}</span>";
       } elseif (strpos($wpicon, 'http') !== false || strpos($wpicon, 'data:') !== false) {
         ///ICON IS IMAGE
-
         return "<img src='{$wpicon}' class='uip-icon-image uip-border-round uip-w-16 uip-ratio-1-1 uip-filter-contrast'>";
       } else {
         ///ICON IS ::BEFORE ELEMENT
