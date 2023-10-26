1bfe5d7875a260b0d7f3f5f525fab70766d94e7f
wpuipress <120018841+wpuipress@users.noreply.github.com>
2023-10-26 18:57:15 +0100

    Removed error flags

--- uipress-lite.php
+++ uipress-lite.php
@@ -23,7 +23,3 @@ require uip_plugin_path . "admin/uipress-compiler.php";
 
 $uipress = new uipress_compiler();
 $uipress->run();
-
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
