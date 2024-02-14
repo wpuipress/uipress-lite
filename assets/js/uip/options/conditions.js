779c29751059fa871389114897c7597a44d6469b
wpuipress <120018841+wpuipress@users.noreply.github.com>
2024-02-14 12:49:04 +0000

    Enhanced conditions API

--- assets/js/uip/options/conditions.js
+++ assets/js/uip/options/conditions.js
@@ -20,6 +20,9 @@ export default {
         value: __("Value", "uipress-lite"),
         addCondition: __("Add condition", "uipress-lite"),
         editCondition: __("Edit condition", "uipress-lite"),
+        users: __("Users", "uipress-lite"),
+        dateAndTime: __("Date and time", "uipress-lite"),
+        site: __("Site", "uipress-lite"),
       },
       newCondition: {
         type: "userrole",
@@ -37,32 +40,43 @@ export default {
             label: __("Or", "uipress-lite"),
           },
         },
-        types: [
-          {
-            value: "userrole",
-            label: __("User role", "uipress-lite"),
-          },
-          {
-            value: "userlogin",
-            label: __("User login", "uipress-lite"),
-          },
-          {
-            value: "userid",
-            label: __("User ID", "uipress-lite"),
-          },
-          {
-            value: "useremail",
-            label: __("User email", "uipress-lite"),
-          },
-        ],
         operators: {
           is: {
             value: "is",
-            label: __("Is", "uipress-lite"),
+            label: __("Is (===)", "uipress-lite"),
+            shortHand: "===",
           },
           isnot: {
             value: "isnot",
-            label: __("Is not", "uipress-lite"),
+            label: __("Is not (!=)", "uipress-lite"),
+            shortHand: "!==",
+          },
+          contains: {
+            value: "contains",
+            label: __("Contains", "uipress-lite"),
+            shortHand: "contains",
+          },
+        },
+        numericalOperators: {
+          greaterThan: {
+            value: "greaterThan",
+            label: __("Greater than (>)", "uipress-lite"),
+            shortHand: ">",
+          },
+          greaterThanEqualTo: {
+            value: "greaterThanEqualTo",
+            label: __("Greater than or equal to (>=)", "uipress-lite"),
+            shortHand: ">=",
+          },
+          lessThan: {
+            value: "lessThan",
+            label: __("Less than (<)", "uipress-lite"),
+            shortHand: "<",
+          },
+          lessThanEqualTo: {
+            value: "lessThanEqualTo",
+            label: __("Less than or equal to (<=)", "uipress-lite"),
+            shortHand: "<=",
           },
         },
       },
@@ -98,6 +112,109 @@ export default {
   },
   computed: {
     /**
+     * Returns list of all operators
+     *
+     * @since 3.3.095
+     */
+    returnAllOperators() {
+      return { ...this.conditions.operators, ...this.conditions.numericalOperators };
+    },
+
+    /**
+     * Returns list of user types conditions
+     *
+     * @since 3.3.095
+     */
+    returnUserTypes() {
+      return [
+        {
+          value: "userrole",
+          label: __("User role", "uipress-lite"),
+        },
+        {
+          value: "userlogin",
+          label: __("User login", "uipress-lite"),
+        },
+        {
+          value: "userid",
+          label: __("User ID", "uipress-lite"),
+        },
+        {
+          value: "useremail",
+          label: __("User email", "uipress-lite"),
+        },
+      ];
+    },
+
+    /**
+     * Returns list of date types conditions
+     *
+     * @since 3.3.095
+     */
+    returnDateTypes() {
+      return [
+        {
+          value: "weekday",
+          label: __("Weekday (EN)", "uipress-lite"),
+        },
+        {
+          value: "date",
+          label: __("Date (YYYY-MM-DD)", "uipress-lite"),
+        },
+        {
+          value: "time",
+          label: __("Time (HH:MM)", "uipress-lite"),
+        },
+      ];
+    },
+
+    /**
+     * Returns list of site types conditions
+     *
+     * @since 3.3.095
+     */
+    returnSiteTypes() {
+      return [
+        {
+          value: "siteTitle",
+          label: __("Site title", "uipress-lite"),
+        },
+        {
+          value: "siteURL",
+          label: __("Site URL", "uipress-lite"),
+        },
+        {
+          value: "adminURL",
+          label: __("Admin URL", "uipress-lite"),
+        },
+        {
+          value: "currentPageName",
+          label: __("Current page name", "uipress-lite"),
+        },
+        {
+          value: "currentURL",
+          label: __("Current URL", "uipress-lite"),
+        },
+        {
+          value: "locale",
+          label: __("Language (en-US)", "uipress-lite"),
+        },
+        {
+          value: "pluginActive",
+          label: __("Plugin is active (uipress-lite)", "uipress-lite"),
+        },
+        {
+          value: "notificationCount",
+          label: __("Notification count", "uipress-lite"),
+        },
+        {
+          value: "darkMode",
+          label: __("Dark mode (true | false)", "uipress-lite"),
+        },
+      ];
+    },
+
+    /**
      * Returns whether there are any conditions
      *
      * @returns {boolean}
@@ -122,6 +239,23 @@ export default {
   },
   methods: {
     /**
+     * Returns list of operators depending on value of
+     *
+     * @param {String} type - the current type of comparison
+     *
+     * @since 3.3.095
+     */
+    returnOperators(type) {
+      const numericalTypes = ["date", "time", "userid", "notificationCount"];
+
+      // Numerical operators
+      if (numericalTypes.includes(type)) return { ...this.conditions.operators, ...this.conditions.numericalOperators };
+
+      // Standard operators
+      return this.conditions.operators;
+    },
+
+    /**
      * Injects prop value into component
      *
      * @since 3.2.13
@@ -140,7 +274,8 @@ export default {
      * @since 3.2.13
      */
     async addCondition() {
-      this.option.conditions.push(this.newCondition);
+      const newCondition = JSON.parse(JSON.stringify(this.newCondition));
+      this.option.conditions.push(newCondition);
       await this.$nextTick();
       const opener = () => {
         const newIndex = this.option.conditions.length - 1;
@@ -159,8 +294,28 @@ export default {
       this.option.conditions.splice(index, 1);
     },
 
-    handleUserReturn(d, element) {
-      element.value = d;
+    /**
+     * Returns user value from user role search
+     *
+     * @param {String} d - the user value
+     * @param {Object} condition - The condition to update
+     *
+     * @since 3.2.13
+     */
+    handleUserReturn(d, condition) {
+      condition.value = d;
+    },
+
+    /**
+     * Shows user search conditionally
+     *
+     * @param {String} type - the type of condition
+     *
+     * @since 3.3.095
+     */
+    maybeShowUserSearch(type) {
+      const exists = this.returnUserTypes.find((p) => p.value === type);
+      return exists ? true : false;
     },
   },
   template: `
@@ -173,8 +328,8 @@ export default {
 		  <template v-slot:trigger>
 			
 			<div class="uip-flex uip-flex-row uip-gap-xxs">
-			  <div class="uip-padding-xxs uip-border-rounder uip-text-s uip-background-muted uip-flex-grow">
-				{{element.type}} {{element.operator}} {{element.value}}
+			  <div class="uip-padding-xxs uip-border-rounder uip-text-s uip-background-muted uip-flex-grow uip-text-ellipsis uip-no-wrap uip-overflow-hidden uip-max-w-160">
+				{{element.type}} {{returnAllOperators[element.operator].shortHand}} {{element.value}}
 			  </div>
 			  
 			  <button class="uip-button-default uip-border-rounder uip-icon uip-padding-xxs uip-link-muted"
@@ -194,24 +349,57 @@ export default {
 			
 			  <div class="uip-grid-col-1-3">
 			  
-				  <div class="uip-text-muted uip-flex uip-flex-center"><span>{{strings.type}}</span></div>
+				  <div class="uip-text-muted uip-flex uip-flex-center uip-text-s"><span>{{strings.type}}</span></div>
 				  <div class="uip-flex uip-flex-center">
-					<select class="uip-input-small uip-padding-top-xxxs uip-padding-bottom-xxxs uip-max-w-100p uip-w-100p uip-border-rounder" v-model="element.type" style="padding-top: 2px; padding-bottom: 2px; border-radius: var(--uip-border-radius-large);">
-					  <template v-for="item in conditions.types">
-						<option :value="item.value">{{item.label}}</option>
-					  </template>
+          
+					<select v-model="element.type" 
+                    class="uip-input uip-input-small uip-padding-top-xxxs uip-padding-bottom-xxxs uip-max-w-100p uip-w-100p uip-border-rounder" 
+                    style="padding-top: 2px; padding-bottom: 2px; border-radius: var(--uip-border-radius-large);">
+          
+                      <!--Users-->
+                      <optgroup :label="strings.users">
+					    <template v-for="item in returnUserTypes">
+						  <option :value="item.value">{{item.label}}</option>
+					    </template>
+                      </optgroup>
+                      
+                      
+                      <!--Date & time-->
+                      <optgroup :label="strings.dateAndTime">
+                        <template v-for="item in returnDateTypes">
+                        <option :value="item.value">{{item.label}}</option>
+                        </template>
+                      </optgroup>
+                      
+                      <!--Site-->
+                      <optgroup :label="strings.site">
+                        <template v-for="item in returnSiteTypes">
+                        <option :value="item.value">{{item.label}}</option>
+                        </template>
+                      </optgroup>
+                      
 					</select>
+          
 				  </div>
 				  
-				  <div class="uip-text-muted uip-flex uip-flex-center"><span>{{strings.operator}}</span></div>
-				  <div class="uip-flex uip-flex-center">
-					<toggle-switch :options="conditions.operators" :activeValue="element.operator" :returnValue="function(data){ element.operator = data;}"></toggle-switch>
-				  </div>
+				  <div class="uip-text-muted uip-flex uip-flex-center uip-text-s"><span>{{strings.operator}}</span></div>
+          
+                  <select class="uip-input uip-input-small uip-padding-top-xxxs uip-padding-bottom-xxxs uip-max-w-100p uip-w-100p uip-border-rounder" 
+                  v-model="element.operator" style="padding-top: 2px; padding-bottom: 2px; border-radius: var(--uip-border-radius-large);">
+                  
+                    <template v-for="item in returnOperators(element.type)">
+                      <option :value="item.value">{{item.label}}</option>
+                    </template>
+                    
+                  </select>  
+          
 				  
-				  <div class="uip-text-muted uip-flex uip-flex-center"><span>{{strings.value}}</span></div>
+				  <div class="uip-text-muted uip-flex uip-flex-center uip-text-s"><span>{{strings.value}}</span></div>
 				  <div class="uip-flex uip-flex-center uip-gap-xxs">
 					<input type="text" class="uip-input-small uip-flex-grow" style="min-width:1px" v-model="element.value">
-					<user-role-search :selected="[]" :returnType="element.type" :searchPlaceHolder="strings.searchUsers" :updateSelected="(d)=>handleUserReturn(d, element)"></user-role-search>
+					<user-role-search 
+                    v-if="maybeShowUserSearch(element.type)"
+                    :selected="[]" :returnType="element.type" :searchPlaceHolder="strings.searchUsers" :updateSelected="(d)=>handleUserReturn(d, element)"></user-role-search>
 				  </div>
 				  
 			  </div>    
