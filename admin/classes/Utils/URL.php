2f97e91008ff78b98a9788d6b070b02128ad51fd
wpuipress <120018841+wpuipress@users.noreply.github.com>
2024-02-15 14:36:51 +0000

    Fixed fatal with iconv being undefined

--- admin/classes/Utils/URL.php
+++ admin/classes/Utils/URL.php
@@ -1,7 +1,7 @@
 <?php
 namespace UipressLite\Classes\Utils;
 
-!defined('ABSPATH') ? exit() : '';
+!defined("ABSPATH") ? exit() : "";
 
 class URL
 {
@@ -14,22 +14,22 @@ class URL
   public static function current()
   {
     //Clean input
-    $uri = sanitize_url($_SERVER['REQUEST_URI']);
-    $host_s = '';
-    if (isset($_SERVER['HTTPS'])) {
-      $host_s = sanitize_url($_SERVER['HTTPS']);
+    $uri = sanitize_url($_SERVER["REQUEST_URI"]);
+    $host_s = "";
+    if (isset($_SERVER["HTTPS"])) {
+      $host_s = sanitize_url($_SERVER["HTTPS"]);
     }
-    $port = '';
-    if (isset($_SERVER['SERVER_PORT'])) {
-      $port = sanitize_url($_SERVER['SERVER_PORT']);
+    $port = "";
+    if (isset($_SERVER["SERVER_PORT"])) {
+      $port = sanitize_url($_SERVER["SERVER_PORT"]);
     }
-    $http_host = '';
-    if (isset($_SERVER['HTTP_HOST'])) {
-      $http_host = sanitize_url($_SERVER['HTTP_HOST']);
+    $http_host = "";
+    if (isset($_SERVER["HTTP_HOST"])) {
+      $http_host = sanitize_url($_SERVER["HTTP_HOST"]);
     }
 
     //Build protocol
-    $protocol = (!empty($host_s) && $host_s != 'off') || $port == 443 ? 'https://' : 'http://';
+    $protocol = (!empty($host_s) && $host_s != "off") || $port == 443 ? "https://" : "http://";
     $url = $protocol . $http_host . $uri;
 
     return $url;
@@ -41,28 +41,28 @@ class URL
    * @since 3.0.0
    * Credit: from https://stackoverflow.com/questions/2955251/php-function-to-make-slug-url-string
    */
-  public static function urlSafe($text, $divider = '-')
+  public static function urlSafe($text, $divider = "-")
   {
     // replace non letter or digits by divider
-    $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+    $text = preg_replace("~[^\pL\d]+~u", $divider, $text);
 
     // transliterate
-    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    $text = function_exists("iconv") ? iconv("utf-8", "us-ascii//TRANSLIT", $text) : $text;
 
     // remove unwanted characters
-    $text = preg_replace('~[^-\w]+~', '', $text);
+    $text = preg_replace("~[^-\w]+~", "", $text);
 
     // trim
     $text = trim($text, $divider);
 
     // remove duplicate divider
-    $text = preg_replace('~-+~', $divider, $text);
+    $text = preg_replace("~-+~", $divider, $text);
 
     // lowercase
     $text = strtolower($text);
 
     if (empty($text)) {
-      return 'n-a';
+      return "n-a";
     }
 
     return $text;
